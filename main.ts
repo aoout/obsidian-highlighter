2f2fc45751a98ad3daf96a6b6c8d50bc0a5acad4
aoout <wuz66280@gmail.com>
2023-09-22 15:33:20 +0800

    first commit.

--- main.ts
+++ main.ts
@@ -1,134 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin, TFile } from "obsidian";
+import { Highlight, HighlightParser } from "src/highlightParser";
+import { HighlightModal } from "src/highlightModal";
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: "find-highlights",
+			name: "Find highlights on active file",
+			callback: async () => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
+					// read the content of activeFile
+					this.app.vault.cachedRead(activeFile).then((content) => {
+						const parser = new HighlightParser(content);
+						const highlights = parser.highlights;
+						new HighlightModal(this.app, highlights).open();
+					});
 				}
-			}
+			},
 		});
+		this.addCommand({
+			id: "find-highlights-from-moc",
+			name: "Find highlights on active file as Moc",
+			callback: async () => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
+					const highlights: Highlight[] = [];
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+					for (const item of this.app.metadataCache.getFileCache(activeFile)?.links || []) {
+						const file = this.app.vault.getAbstractFileByPath(
+							item.link + ".md"
+						);
+						if (file && file instanceof TFile) {
+							await this.app.vault
+								.cachedRead(file)
+								.then((content) => {
+									const parser = new HighlightParser(
+										content
+									);
+									console.log(parser.highlights);
+									parser.highlights.forEach(
+										(highlight) => {
+											highlight.noteLink = item.link;
+											highlights.push(highlight);
+										}
+									);
+								});
+						}
+					}
+					console.log(highlights);
+					new HighlightModal(this.app, highlights).open();
+				}
+			},
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	onunload() {}
 }
---
+++ src/highlightModal.ts
@@ -0,0 +1,51 @@
+import { App, SuggestModal, TFile } from "obsidian";
+import { Highlight } from "./highlightParser";
+
+export class HighlightModal extends SuggestModal<Highlight> {
+	app: App;
+	highlight: Highlight[];
+	getSuggestions(query: string): Highlight[] | Promise<Highlight[]> {
+		return this.highlight.filter((h) => h.content.includes(query));
+	}
+	renderSuggestion(item: Highlight, el: HTMLElement): void {
+		// get the basename of item.notelink
+		if (item.noteLink) {
+			const basename = item.noteLink?.split("/").pop();
+			el.createEl("div", { text: basename });
+		}
+		el.createEl("small", { text: item.content });
+	}
+	onChooseSuggestion(
+		item: Highlight,
+		evt: MouseEvent | KeyboardEvent
+	): boolean {
+		if (item.noteLink) {
+			const file = this.app.vault.getAbstractFileByPath(
+				item.noteLink + ".md"
+			);
+			if (file && file instanceof TFile) {
+				this.app.workspace
+					.getLeaf()
+					.openFile(file)
+					.then(() => {
+						this.app.workspace.activeEditor?.editor?.scrollIntoView(
+							item.range,
+							true
+						);
+					});
+			}
+		} else {
+			this.app.workspace.activeEditor?.editor?.scrollIntoView(
+				item.range,
+				true
+			);
+		}
+
+		return true;
+	}
+	constructor(app: App, highlights: Highlight[]) {
+		super(app);
+		this.app = app;
+		this.highlight = highlights;
+	}
+}
