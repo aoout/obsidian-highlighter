fe7cf0411293c2eb87c1d5255e27e9e1436db87f
aoout <wuz66280@gmail.com>
2023-09-22 19:05:30 +0800

    comment.

--- main.ts
+++ main.ts
@@ -1,9 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Plugin, TFile } from "obsidian";
 import { Highlight, HighlightParser } from "src/highlightParser";
 import { HighlightModal } from "src/highlightModal";
+import { ExportHignlightParser } from "src/exportHighlightParser";
 
 export default class MyPlugin extends Plugin {
 	async onload() {
+		this.addCommand({
+			id: "highlight-selected",
+			name: "Highlight selected text",
+			callback: () => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
+					const editor = this.app.workspace.activeEditor?.editor;
+					if (editor) {
+						const selectedText = editor.getSelection();
+						this.app.vault
+							.cachedRead(activeFile)
+							.then((content) => {
+								const newContent = content.replace(
+									selectedText,
+									"==" + selectedText + "=="
+								);
+								this.app.vault.modify(activeFile, newContent);
+							});
+					}
+				}
+			},
+		});
+		this.addCommand({
+			id: "clear-highlights",
+			name: "Clear hightlights on active file",
+			callback: () => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
+					this.app.vault.cachedRead(activeFile).then((content) => {
+						const newContent = content.replace(/==(.+)==/g, "$1");
+						this.app.vault.modify(activeFile, newContent);
+					});
+				}
+			},
+		});
 		this.addCommand({
 			id: "find-highlights",
 			name: "Find highlights on active file",
@@ -21,38 +58,85 @@ export default class MyPlugin extends Plugin {
 		});
 		this.addCommand({
 			id: "find-highlights-from-moc",
-			name: "Find highlights on active file as Moc",
+			name: "Find highlights on active file as MOC",
 			callback: async () => {
-				const activeFile = this.app.workspace.getActiveFile();
-				if (activeFile) {
-					const highlights: Highlight[] = [];
-
-					for (const item of this.app.metadataCache.getFileCache(activeFile)?.links || []) {
-						const file = this.app.vault.getAbstractFileByPath(
-							item.link + ".md"
-						);
-						if (file && file instanceof TFile) {
-							await this.app.vault
-								.cachedRead(file)
-								.then((content) => {
-									const parser = new HighlightParser(
-										content
-									);
-									console.log(parser.highlights);
-									parser.highlights.forEach(
-										(highlight) => {
-											highlight.noteLink = item.link;
-											highlights.push(highlight);
-										}
-									);
-								});
-						}
-					}
-					console.log(highlights);
+				const highlights = await this.getHighlightsFromMOC();
+				if (highlights) {
 					new HighlightModal(this.app, highlights).open();
 				}
 			},
 		});
+
+		this.addCommand({
+			id: "export-highlights",
+			name: "Export highlights on active file as MOC",
+			callback: async () => {
+				const highlights = await this.getHighlightsFromMOC();
+				if (highlights) {
+					const activeFile = this.app.workspace.getActiveFile();
+					if (activeFile) {
+						const parser = new ExportHignlightParser("");
+						highlights.forEach((highlight) => {
+							parser.addHighlight(
+								highlight.content,
+								highlight.noteLink?.split("/").pop() || ""
+							);
+						});
+
+						const dir = activeFile.path.substring(
+							0,
+							activeFile.path.lastIndexOf("/")
+						);
+						const path = `${dir}/exported-highlights.md`;
+						const file = this.app.vault.getAbstractFileByPath(path);
+						// 如果已经存在该文件，则读取其内容，也放进解析器里
+						if (file && file instanceof TFile) {
+							await this.app.vault
+								.cachedRead(file)
+								.then((content) => {
+									const oldParser = new ExportHignlightParser(
+										content
+									);
+									// oldParser.merge(parser);
+									// 使用parser.toString更新文件内容
+									this.app.vault.modify(
+										file,
+										oldParser.toString()
+									);
+								});
+						} else {
+							this.app.vault.create(path, parser.toString());
+						}
+					}
+				}
+			},
+		});
+	}
+
+	async getHighlightsFromMOC(): Promise<void | Highlight[]> {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (activeFile) {
+			const highlights: Highlight[] = [];
+
+			for (const item of this.app.metadataCache.getFileCache(activeFile)
+				?.links || []) {
+				const file = this.app.vault.getAbstractFileByPath(
+					item.link + ".md"
+				);
+				if (file && file instanceof TFile) {
+					await this.app.vault.cachedRead(file).then((content) => {
+						const parser = new HighlightParser(content);
+						console.log(parser.highlights);
+						parser.highlights.forEach((highlight) => {
+							highlight.noteLink = item.link;
+							highlights.push(highlight);
+						});
+					});
+				}
+			}
+			console.log(highlights);
+			return highlights;
+		}
 	}
 
 	onunload() {}
---
+++ src/exportHighlightParser.ts
@@ -0,0 +1,97 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+interface Highlight {
+	content: string;
+	comment: string | null;
+}
+
+class Section {
+	title: string;
+	content: string;
+	highlights: Highlight[];
+	constructor(content: string) {
+
+		this.content = content;
+		this.title = content.split("\n")[0].replace(/^# /, "");
+
+		const items = this.content.split("\n\n");
+        // 排除掉items中的第一个
+        items.shift();
+		this.highlights = items.map((item) => {
+			const lines = item.split("@");
+			const content = lines[0];
+			const comment = lines[1];
+
+			return { content, comment };
+		});
+	}
+}
+
+export class ExportHignlightParser {
+	content: string;
+	sections: Section[];
+	constructor(content: string) {
+		// "# 一段文本" 这样的形式代表标题。从一个标题出现，直到下一个标题出现，视为一个section,如此分割内容,并且按照标题文本分组
+		this.content = content;
+		this.sections = [];
+        if(content){
+            content.split("\n").forEach((line) => {
+                if (line.match(/^# /)) {
+                    this.sections.push(new Section(line));
+                } else {
+                    const lastSection = this.sections[this.sections.length - 1];
+                    this.sections[this.sections.length - 1] = new Section(
+                        lastSection.content + "\n" + line
+                    );
+                }
+            });
+            
+        }
+		
+	}
+
+	addHighlight(content: string, sectionTitle: string) {
+		const section = this.sections.find(
+			(section) => section.title === sectionTitle
+		);
+		if (section) {
+			const highlight = section.highlights.find(
+				(highlight) => highlight.content == content
+			);
+			if (!highlight) {
+				section.highlights.push({ content, comment: null });
+			}
+		} else {
+			this.sections.push(new Section(`# ${sectionTitle}\n\n${content}`));
+		}
+	}
+
+    merge(highlightParser: ExportHignlightParser) {
+        highlightParser.sections.forEach(section => {
+            section.highlights.forEach(highlight => {
+                this.addHighlight(highlight.content, section.title)
+            })
+        })
+    }
+
+    toString() {
+        // 需要深入高亮级别进行实现
+        console.log(this.sections);
+        let content = ""
+        this.sections.forEach(section => {
+            content += "# " + section.title + "\n\n"
+            section.highlights.forEach(highlight => {
+                content += highlight.content
+                if(highlight.comment){
+
+                    content += "@" + highlight.comment
+                }
+                content += "\n\n"
+            })
+            // 删除content最后一个字符
+            content = content.slice(0, -1)
+        })
+        
+        return content
+    }
+}
